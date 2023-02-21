321988753f037e88d339d04254d2b561c654431f
SJH0619 <idiotGuy0619@outlook.com>
2023-02-21 13:32:49 +0900

    index 추가

--- blockchain/block/block.py
+++ blockchain/block/block.py
@@ -6,17 +6,20 @@ class Block():
     nonce = 0
     hash = ""
 
-    def __init__(self, previous_hash, transactions):
+    def __init__(self, index: int, previous_hash: bytes, transactions: list):
+        self.index = index
         self.previous_hash = previous_hash
         self.timestamp = time.time()
         self.transactions = transactions
 
     def get_block_information(self):
         transaction_str_list = []
+
         for transaction in self.transactions:
             transaction_str_list.append(str(transaction))
 
         block_info = {
+            "index": self.index,
             "nonce": self.nonce,
             "hash": self.hash,
             "previous hash": self.previous_hash,
--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -5,20 +5,23 @@ class BlockChain():
     chain = [];
     pending_transaction = [];
 
+    def get_index(self):
+        return len(self.chain)+1
+
     def get_lastest_block(self):
         return self.chain[len(self.chain)-1]
     
     async def create_genesis_block(self):
-        genesis_block = Block("0", [])
+        genesis_block = Block(1, "0", [])
         await genesis_block.mine()
 
         self.chain.append(genesis_block)
 
     def create_transaction(self, transaction):
         self.pending_transaction.append(transaction)
-    
+
     async def mine_pending_transaction(self):
-        block = Block(self.get_lastest_block().hash, self.pending_transaction)
+        block = Block(self.get_index(), self.get_lastest_block().hash, self.pending_transaction)
         await block.mine()
 
         self.chain.append(block)
